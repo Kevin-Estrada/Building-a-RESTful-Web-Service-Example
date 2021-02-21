1a115849913fb0cb219bff385aa7d271ccdceb6f
Kevin-Estrada <ke81297@gmail.com>
2021-02-21 01:21:09 -0600

    first commit

---
+++ src/test/java/com/example/kevinestrada/RESTfulWebService/ResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kevinestrada.RESTfulWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
